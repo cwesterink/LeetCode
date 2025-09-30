e0fd203a66df66a31da7267b488e91a3b23aa18f
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-09-29 22:29:50 -0400

    LC-minimum-score-triangulation-of-polygon [Runtime: 47 ms; Memory: 18.2 MB]

--- dynamic-programming/minimum-score-triangulation-of-polygon.py
+++ dynamic-programming/minimum-score-triangulation-of-polygon.py
@@ -1,15 +1,21 @@
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
         length = len(values)
+        memoTable = [[-1 for _ in range(length)] for _ in range(length)]
 
         def helper(start, end):
+            memoRes = memoTable[start][end-1]
+            if (memoRes != -1): return memoRes
+
             n = end - start
 
             if (n < 3):
+                memoTable[start][end-1] = 0
                 return 0
 
             if (n == 3):
-                return values[start] * values[start+1] * values[start+2]
+                memoTable[start][end-1] = values[start] * values[start+1] * values[start+2]
+                return memoTable[start][end-1]
 
             base = values[start] * values[end-1]
             minScore = -1
@@ -22,6 +28,7 @@ class Solution:
                 if score < minScore or minScore == -1:
                     minScore = score
 
+            memoTable[start][end-1] = minScore
             return minScore
 
 
