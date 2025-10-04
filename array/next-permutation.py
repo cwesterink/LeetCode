1725ecf170ada16e4a710e426cba613ebea33331
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-10-03 22:31:56 -0400

    LC-next-permutation [Runtime: N/A; Memory: N/A]

---
+++ array/next-permutation.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+
+        l = len(nums)
+        prev = -1
+        for i in range(l-1, 0, -1):
+            cur = nums[i]
+            if (prev > cur):
+                nums[i], nums[i+1] = prev, cur
+                break
+            prev = cur
+        else:
+            nums.sort()
+        
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
