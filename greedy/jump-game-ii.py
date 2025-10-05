be74e547c054d712f9b604252eafaa32c85f93ef
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-10-05 19:55:15 -0400

    LC-jump-game-ii [Runtime: N/A; Memory: N/A]

--- greedy/jump-game-ii.py
+++ greedy/jump-game-ii.py
@@ -6,11 +6,21 @@ class Solution:
         def helper(i):
             if i == n-1:
                 return 0
-            
+            if i > n-1:
+                return -1
+
             maxDist = nums[i]
             if (maxDist == 0):
-                return 10**4+1
-            bestJump = min([helper(j) for j in range(i+1, i+maxDist+1)])
+                return -1
+
+            bestJump = -1
+            for j in range(i+1, i+maxDist+1):
+                d = helper(j)
+                if d == -1:
+                    continue
+                
+                if d < bestJump or bestJump == -1:
+                    bestJump = d
 
             return 1 + bestJump
         
