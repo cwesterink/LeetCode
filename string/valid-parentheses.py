283103821fc5a7b7dae5088e85b0708deb0d2cbe
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-10-16 22:38:22 -0400

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

---
+++ string/valid-parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        queue = []
+        openP = { '(', '[', '{'}
+        closeP = {')', ']', '}'}
+        for c in s:
+            if c in openP:
+                queue.append(c)
+            else:
+                o = queue.pop()
+                if (o == "(" and c != ")"):
+                    return False
+                if (o == "{" and c != "}"):
+                    return False
+                if (o == "[" and c != "]"):
+                    return False
+        return True
+
+        
