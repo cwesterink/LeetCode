f69a2ad4e593e0b9671e500cc383449213759483
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-10-16 22:38:47 -0400

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -14,6 +14,6 @@ class Solution:
                     return False
                 if (o == "[" and c != "]"):
                     return False
-        return True
+        return len(queue) == 0
 
         
