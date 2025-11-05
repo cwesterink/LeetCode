99f2a6c49bc5cb2d128d4818028476bf9cf2ad3a
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-04 23:42:37 -0500

    LC-validate-binary-search-tree [Runtime: N/A; Memory: N/A]

---
+++ depth-first-search/validate-binary-search-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        
+        return self.validator(root, None, None)
+    
+    def validator(self, root, minVal, maxVal):
+        if (root == None):
+            return True
+        if (maxVal != None and root.val > maxVal) or (minVal != None and root.val < minVal):
+            return False
+        
+        return self.validator(root.left, minVal, root.val) and self.validator(root.right, root.val, maxVal)
