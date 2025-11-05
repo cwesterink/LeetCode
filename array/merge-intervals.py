34eb1a7d0db3e22ca81f71367e45dea4539d7f97
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-05 02:49:18 -0500

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

--- array/merge-intervals.py
+++ array/merge-intervals.py
@@ -4,16 +4,18 @@ class Solution:
         intervals.sort(key=lambda interval: interval[0])
 
         intervalStart = intervals[0][0]
+        intervalEnd =intervals[0][1]
         mergedIntervals = []
         for i in range(len(intervals)-1):
             int1 = intervals[i]
             int2 = intervals[i+1]
+            intervalEnd = max(intervalEnd, int2[1])
             if int1[1] < int2[0]:
                 mergedIntervals.append([intervalStart, int1[1]])
                 intervalStart = int2[0]
 
 
-        mergedIntervals.append([intervalStart, intervals[-1][1]])
+        mergedIntervals.append([intervalStart,intervalEnd])
 
         return mergedIntervals
         
