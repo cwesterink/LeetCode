1c7ab16cda3b7beea47a08624dc225f4f7276d6c
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-05 01:31:48 -0500

    LC-climbing-stairs [Runtime: N/A; Memory: N/A]

--- dynamic-programming/climbing-stairs.py
+++ dynamic-programming/climbing-stairs.py
@@ -1,9 +1,14 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        if n == 2:
-            return 2
 
-        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        memo = [-1 for i in range(n+1)]
+        memo[1] = 1
+        memo[2] = 2
+
+        def helper(i):
+            if memo[i] == -1:
+                memo[i] = helper(n-1) + helper(n-2)
+            return memo[i]
+        
+        return helper(n)
         
