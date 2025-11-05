41df6c237b7444f871b01e956f9f7f12fe2190a2
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-05 01:05:30 -0500

    LC-number-of-islands [Runtime: N/A; Memory: N/A]

---
+++ union-find/number-of-islands.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+
+        n, m  = len(grid), len(grid[0])
+
+        def exploreIsland(i, j):
+            if i >= n or j >= m:
+                return
+
+            if grid[i][j] == '0':
+                return
+            grid[i][j] = '0'
+            exploreIsland(i+1, j)
+            exploreIsland(i-1, j)
+            exploreIsland(i, j+1)
+            exploreIsland(i, j-1)
+
+
+            
+            
+
+        numIslands = 0
+        for i in range(n):
+            for j in range(m):
+                cell = grid[i][j]
+
+                if cell == '1':
+                    numIslands += 1
+                    exploreIsland(i, j)
+
+        print(grid)
+        return numIslands
+
+
+
+        
