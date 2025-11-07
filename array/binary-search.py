641298dcea11a42756e9a124a699f87f9b35f0ab
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-07 00:35:42 -0500

    LC-binary-search [Runtime: N/A; Memory: N/A]

---
+++ array/binary-search.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        i, j = 0, len(nums) - 1
+        while i < j:
+            mid = (j+i) // 2
+
+            midVal = nums[mid]
+            if midVal == target:
+                return mid
+ 
+            if midVal > target:
+                j = mid - 1
+            else:
+                i = mid + 1
+
+        return -1
+        
