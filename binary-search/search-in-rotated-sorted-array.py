a5ed6c6200e21fddd3cc7959595265d0405e8951
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-07 21:43:27 -0500

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ binary-search/search-in-rotated-sorted-array.py
@@ -0,0 +1,36 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        l = len(nums)
+        i, j = 0, len(nums) - 1
+        m = 0
+        while i < j:
+
+            m = (i+j) // 2
+            print(i, m, j)
+            if m +1 < l and nums[m] > nums[m+1]:
+                print("found break")
+                break
+            if nums[m] < nums[i]:
+                j = m
+            else:
+                i = m
+
+        if target > nums[l-1]:
+            # start array
+            i, j = 0, m
+        else:
+            # end array
+            i, j = m + 1, l-1
+
+    
+        while i <= j:
+            m = (i+j) // 2
+            if nums[m] == target:
+                return m
+            if nums[m] > target:
+                j = m - 1
+            else:
+                i = m + 1
+        return -1
+        
