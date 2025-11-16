4aecefd639107cdf011199d28e75b46382b3fe1b
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-16 16:58:17 -0500

    LC-number-of-substrings-with-only-1s [Runtime: 23 ms; Memory: 18.1 MB]

--- math/number-of-substrings-with-only-1s.py
+++ math/number-of-substrings-with-only-1s.py
@@ -1,6 +1,5 @@
 class Solution:
     def numSub(self, s: str) -> int:
-
         cap = 10**9 + 7
         streak = 0
         res = 0
@@ -13,7 +12,6 @@ class Solution:
                 streak += 1
         
         res += (streak * (streak + 1)//2) % cap
-
         return res % cap
 
         
