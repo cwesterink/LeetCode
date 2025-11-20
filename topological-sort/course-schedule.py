ccc1083f4e5382bd24a4dd8c3e980f1f6e710604
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-19 22:08:22 -0500

    LC-course-schedule [Runtime: N/A; Memory: N/A]

---
+++ topological-sort/course-schedule.py
@@ -0,0 +1,42 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        dependencyGraph = {}
+
+        for clss, prereq in prerequisites:
+            if prereq not in dependencyGraph:
+                dependencyGraph[prereq] = []
+            dependencyGraph[prereq].append(clss)
+        
+        whiteSet = set([x for x in range(numCourses)])
+        greySet = set()
+        blackSet = set()
+
+        def checkCycle(node):
+            if node in greySet:
+                return True
+            if node in blackSet:
+                return False
+            
+            whiteSet.remove(node)
+            greySet.add(node)
+            if node not in dependencyGraph:
+                return False
+            for n in dependencyGraph[node]:
+                if checkCycle(n):
+                    return True
+            
+            greySet.remove(node)
+            blackSet.add(node)
+            return False
+
+        while len(whiteSet) > 0:
+            node = whiteSet.pop()
+            whiteSet.add(node)
+            if checkCycle(node):
+                return False
+        return True
+
+            
+
+        
