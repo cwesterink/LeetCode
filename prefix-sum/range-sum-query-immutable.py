ac222f64c79f4ee5cfb4705a672c9f9911a17ad1
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-22 15:56:31 -0500

    LC-range-sum-query-immutable [Runtime: 0 ms; Memory: 22 MB]

---
+++ prefix-sum/range-sum-query-immutable.py
@@ -0,0 +1,19 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.rollingSum = []
+        s = 0
+        for i in nums:
+            s += i
+            self.rollingSum.append(s)
+        
+    def sumRange(self, left: int, right: int) -> int:
+        if left == 0:
+            return self.rollingSum[right]
+        return self.rollingSum[right] - self.rollingSum[left-1]
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
