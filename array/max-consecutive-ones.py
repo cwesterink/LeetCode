273e15d1d059be0d707121a82ae24aeb84003c79
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-22 16:12:54 -0500

    LC-max-consecutive-ones [Runtime: N/A; Memory: N/A]

---
+++ array/max-consecutive-ones.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        best = 0
+        streak = 0
+        nums.append(0)
+        for val in nums:
+            if val == 0 and streak > 0:
+                if streak > best:
+                    best = streak
+                streak = 0
+            else:
+                streak += 1
+        return best
+            
+        
