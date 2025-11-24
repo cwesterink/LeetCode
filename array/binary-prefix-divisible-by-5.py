f1a0029cad8743c7a5836c21ef60bccdafc7a4e1
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-23 22:38:32 -0500

    LC-binary-prefix-divisible-by-5 [Runtime: 105 ms; Memory: 19.2 MB]

--- array/binary-prefix-divisible-by-5.py
+++ array/binary-prefix-divisible-by-5.py
@@ -1,4 +1,9 @@
 class Solution:
     def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
-        return [(x % 5) == 0 for x in nums]
+        prev = 0
+        res = []
+        for i in nums:
+            prev = prev * 2 + i
+            res.append(prev % 5 == 0)
+        return res
         
