c848c46aeca5563783cfae0e86a3a1cb7961f878
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-11-30 22:29:22 -0500

    LC-insert-interval [Runtime: N/A; Memory: N/A]

---
+++ array/insert-interval.py
@@ -0,0 +1,30 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+
+        res = []
+        activeMerge = False
+
+        for l, r in intervals:
+            print(l, r, activeMerge)
+            if activeMerge:
+                if newInterval[1] < l:
+                    res.append(newInterval)
+                    activeMerge = False
+                    res.append([l,r])
+                elif newInterval[1] < r:
+                    newInterval[1] = r
+            else:
+                if newInterval[0] <= r:
+                    if newInterval[1] <= r:
+                        res.append([l, r])
+                    else:
+                        newInterval[0] = l
+                        activeMerge = True
+                else:
+                    res.append([l, r])
+
+        return res
+
+
+
+        
