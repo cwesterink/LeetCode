8d8b708218b780feda1c559811e900647e4d7c08
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-12-02 02:26:36 -0500

    LC-count-days-without-meetings [Runtime: N/A; Memory: N/A]

---
+++ sorting/count-days-without-meetings.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+
+        meetings.sort(key=lambda x: x[0])
+        meetings.append([days+1, days+1])
+        # print(meetings)
+        res = 0
+        prev = 1
+        for start, end in meetings:
+            res += max(0, start-prev-1)
+            # print(res)
+            prev = end
+        return res
