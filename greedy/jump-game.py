eb0c903e1b32a5a1f1efa30ef43d4e905d28d976
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-12-04 03:07:29 -0500

    LC-jump-game [Runtime: 15 ms; Memory: 18.5 MB]

--- greedy/jump-game.py
+++ greedy/jump-game.py
@@ -2,16 +2,17 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
 
         n = len(nums)
-        
 
-        def helper(i):
-            if i >= n-1:
-                return True
-            
-            for j in range(i+1, i + nums[i]+ 1):
-                if helper(j):
-                    return True
-            return False
+        nums[-1] = 1
+        dist = 1
 
-        return helper(0)
+        i = n-2
+        while i >= 0:
+            if dist > nums[i]:
+                dist += 1
+            else:
+                dist = 1
+            i -= 1
+        return dist == 1
+
         
