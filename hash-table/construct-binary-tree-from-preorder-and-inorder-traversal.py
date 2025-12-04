864fb8b027052dd14872d7fee5826187b6b48630
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-12-04 02:16:09 -0500

    LC-construct-binary-tree-from-preorder-and-inorder-traversal [Runtime: 6 ms; Memory: 19.3 MB]

---
+++ hash-table/construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
+        inorderIndexMap = {}
+        x = [0]
+        for i, val in enumerate(inorder):
+            inorderIndexMap[val] = i
+
+        def buildFromRange(a, b):
+            if a == b:
+                return None
+            
+            print(a, b)
+            val = preorder[x[0]]
+            x[0] += 1
+            res = TreeNode(val)
+            split = inorderIndexMap[val]
+
+            res.left = buildFromRange(a, split)
+            res.right = buildFromRange(split+1, b)
+
+            return res
+
+        return buildFromRange(0, len(preorder))
+
+
+        
