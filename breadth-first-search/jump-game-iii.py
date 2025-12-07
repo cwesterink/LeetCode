917b7fd09c6479edad1ca05ec66b51bb854b7d19
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-12-07 14:24:22 -0500

    LC-jump-game-iii [Runtime: 9 ms; Memory: 23 MB]

--- breadth-first-search/jump-game-iii.py
+++ breadth-first-search/jump-game-iii.py
@@ -14,22 +14,20 @@ class Solution:
         visited = { start }
         
 
-        while len(queue) > 0:
-            frontier = []
-            for i in range(len(queue)):
-                idx = queue.popleft()
-                val = arr[idx]
-                if arr[idx] == 0:
-                    return True
+        while queue:
+            idx = queue.popleft()
+            val = arr[idx]
+            if arr[idx] == 0:
+                return True
 
-                
-                l, r = idx - val, idx + val
-                if inBounds(l) and l not in visited:
-                    queue.append(l)
-                    visited.add(l)
-                if inBounds(r) and r not in visited:
-                    queue.append(r)
-                    visited.add(r)
+            
+            l, r = idx - val, idx + val
+            if inBounds(l) and l not in visited:
+                queue.append(l)
+                visited.add(l)
+            if inBounds(r) and r not in visited:
+                queue.append(r)
+                visited.add(r)
 
         return False
 
