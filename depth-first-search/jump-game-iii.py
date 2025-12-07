c65bdb139c0bb0a40afc599842c8415c23bed20e
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-12-07 14:19:53 -0500

    LC-jump-game-iii [Runtime: 19 ms; Memory: 22.7 MB]

--- depth-first-search/jump-game-iii.py
+++ depth-first-search/jump-game-iii.py
@@ -5,45 +5,29 @@ class Solution:
 
         #Build graph
 
-        graph = {}
         n = len(arr)
 
         def inBounds(i):
             return i >= 0 and i < n
-
-        sink = set()
-        for i, val in enumerate(arr):
-            if val == 0:
-                sink.add(i)
-                continue
-            neighbors = []
-            graph[i] = neighbors
-
-            if inBounds(i+val):
-                neighbors.append(i+val)
-            if inBounds(i-val):
-                neighbors.append(i-val)
         
         queue = deque([start])
         visited = set()
-        print(graph)
-        def getNewNeighbors(i):
-            res = []
-            for neighbor in graph[i]:
-                if neighbor not in visited:
-                    res.append(neighbor)
-            return res
+        
 
         while len(queue) > 0:
             frontier = []
             for i in range(len(queue)):
-                node = queue.popleft()
-                if node in sink:
+                idx = queue.popleft()
+                val = arr[idx]
+                if arr[idx] == 0:
                     return True
 
-                frontier.extend(getNewNeighbors(node))
-                visited.add(node)
-            queue.extend(frontier)
+                visited.add(idx)
+                l, r = idx - val, idx + val
+                if inBounds(l) and l not in visited:
+                    queue.append(l)
+                if inBounds(r) and r not in visited:
+                    queue.append(r)
 
         return False
 
