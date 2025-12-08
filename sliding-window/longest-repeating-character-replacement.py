f6962450f94139b4c5933dfcacf0bdae0df139e4
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-12-07 19:01:16 -0500

    LC-longest-repeating-character-replacement [Runtime: N/A; Memory: N/A]

---
+++ sliding-window/longest-repeating-character-replacement.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        longest = 0
+        n = len(s)
+        firstChange = -1
+        start, end = 0, 0
+        replacementsUsed = 0
+        while end < n:
+            if s[end] != s[start]:
+                if firstChange == -1:
+                    firstChange = end
+                replacementsUsed += 1
+                if replacementsUsed > k:
+                    longest = max(longest, end-start)
+                    replacementsUsed = 0
+                    start = end = firstChange
+                    firstChange = -1
+            end += 1
+        longest = max(longest, end-start)
+        return longest
+        
+            
+
