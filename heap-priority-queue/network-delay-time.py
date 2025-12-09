719fd32c7bf62f99b66bb342b3b709aa2f6c322f
Constantine <74222402+cwesterink@users.noreply.github.com>
2025-12-09 16:03:43 -0500

    LC-network-delay-time [Runtime: 607 ms; Memory: 20.4 MB]

---
+++ heap-priority-queue/network-delay-time.py
@@ -0,0 +1,38 @@
+import heapq
+import math
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+
+        graph = { i:[] for i in range(1, n+1)}
+
+        for s,d,t in times:
+            graph[s].append((d, t))
+        print(graph)
+
+        delay = {}
+        for i in range(1, n+1):
+            delay[i] = math.inf
+        visited = { k }
+
+        heap = [(0,k)]
+        heapq.heapify(heap)
+
+        while heap:
+            for _ in range(len(heap)):
+                d, i = heapq.heappop(heap)
+                if d < delay[i]:
+                    delay[i] = d
+                
+                for j, dist in graph[i]:
+                    if j not in visited:
+                        visited.add(i)
+                        heapq.heappush(heap, (d+dist, j))
+
+        res = max(delay.values())
+        
+        if res == math.inf:
+            return -1
+        return res
+
+        
